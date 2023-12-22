c404dc382e5ddb4df17f12441f1a1b9f827ef85e
ttran375 <137940945+ttran375@users.noreply.github.com>
2023-12-22 01:53:15 +0000

    Properties

--- Start/Ch1/PropsMethods/Program.cs
+++ Start/Ch1/PropsMethods/Program.cs
@@ -1,13 +1,19 @@
 ﻿// Example file for Advanced C#: Object Oriented Programming by Joe Marini
 
 // Create some rectangle objects
-Rectangle rect1 = new Rectangle(10,20);
+Rectangle rect1 = new Rectangle(10, 20);
 Rectangle rect2 = new Rectangle(30);
 
 Console.WriteLine(rect1.GetArea());
 Console.WriteLine(rect2.GetArea());
 
 // TODO: Operate on the Properties
-
+rect1.BorderSize = 5;
+System.Console.WriteLine($"{rect1.BorderSize}");
+rect1.Width = 5;
+rect1.Height = 6;
+System.Console.WriteLine(rect1.GetArea());
 
 // TODO: Try setting an invalid value
+rect1.Height = -30;
+System.Console.WriteLine(rect1.GetArea());
