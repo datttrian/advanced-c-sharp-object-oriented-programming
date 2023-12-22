ad81a22cb5cd7cc02e4a521fbb17e8acf7f071ec
ttran375 <137940945+ttran375@users.noreply.github.com>
2023-12-22 19:08:43 +0000

    Read-only members

--- Start/Ch2/ReadonlyProps/ReadOnly.cs
+++ Start/Ch2/ReadonlyProps/ReadOnly.cs
@@ -1,25 +1,29 @@
 // Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Using the readonly modifier for class members
 
-public class Book {
+public class Book
+{
     // Declare some private fields
-    private string _ISBN = "";
+    private readonly string _ISBN = "";
     private string _title = "";
     private string _author = "";
 
-    public Book(string ISBN, string Title, string Author) {
+    public Book(string ISBN, string Title, string Author)
+    {
         _ISBN = ISBN;
         _title = Title;
         _author = Author;
     }
 
-    public void Update(string ISBN, string Title, string Author) {
-        _ISBN = ISBN;
+    public void Update(string ISBN, string Title, string Author)
+    {
+        // _ISBN = ISBN;
         _title = Title;
         _author = Author;
     }
 
-    public override string ToString() {
+    public override string ToString()
+    {
         return $"{_ISBN}: {_title} by {_author}";
     }
 }
