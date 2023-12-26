6b4b2c249e878de485a36e8d597f8a9bc71308b1
ttran375 <137940945+ttran375@users.noreply.github.com>
2023-12-26 19:09:54 +0000

    Solution: Add static members to employee

--- Start/Ch3/Challenge/Employees.cs
+++ Start/Ch3/Challenge/Employees.cs
@@ -1,22 +1,38 @@
 // Example file for Advanced C#: Object Oriented Programming by Joe Marini
 // Solution to the Employee Class challenge
 
-public class Employee {
-    public Employee() {}
+public class Employee
+{
+    private static int _empCount = 0;
+    protected static int IDStart;
 
-    public required int ID {get; init;}
-    public required string Department {get; set;}
-    public required string FullName {get; set;}
+    static Employee()
+    {
+        IDStart = 1000;
+    }
 
-    public virtual void AdjustPay(decimal percentage) {}
+    public Employee()
+    {
+        Employee._empCount++;
+        ID = Employee.IDStart++;
+    }
+
+    public static int EmployeeCount { get => _empCount; }
+
+    public int ID { get; init; }
+    public required string Department { get; set; }
+    public required string FullName { get; set; }
+
+    public virtual void AdjustPay(decimal percentage) { }
 
     public override string ToString() => $"{ID}:{FullName}, {Department} ";
 }
 
-public class HourlyEmployee : Employee {
-    public HourlyEmployee() {}
+public class HourlyEmployee : Employee
+{
+    public HourlyEmployee() { }
 
-    public decimal PayRate {get; set;}
+    public decimal PayRate { get; set; }
 
     public override void AdjustPay(decimal percentage)
     {
@@ -24,12 +40,14 @@ public class HourlyEmployee : Employee {
     }
 }
 
-public class SalariedEmployee : Employee {
-    public SalariedEmployee() {}
+public class SalariedEmployee : Employee
+{
+    public SalariedEmployee() { }
 
-    public decimal Salary {get; set;}
+    public decimal Salary { get; set; }
 
-    public override void AdjustPay(decimal percentage) {
+    public override void AdjustPay(decimal percentage)
+    {
         Salary += (Salary * percentage);
     }
 }
