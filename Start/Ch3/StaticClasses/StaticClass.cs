482923a9ca5b737a72c70c0267854eb4e20e9971
ttran375 <137940945+ttran375@users.noreply.github.com>
2023-12-26 19:03:09 +0000

    Static classes

--- Start/Ch3/StaticClasses/StaticClass.cs
+++ Start/Ch3/StaticClasses/StaticClass.cs
@@ -6,14 +6,23 @@
 // They contain only static members (methods, properties, constants)
 // They cannot be instantiated, and are sealed by default
 // (so they cannot be derived from)
-public class Converter {
+public static class Converter
+{
     public static double INCH_CM_CONVERT = 2.54;
+    // public int temp;
 
-    public static double InToCm(double inches) {
+    public static double InToCm(double inches)
+    {
         return inches * INCH_CM_CONVERT;
     }
 
-    public static double CmToIn(double centimeters) {
+    public static double CmToIn(double centimeters)
+    {
         return centimeters / INCH_CM_CONVERT;
     }
 }
+
+// public static class NewConv : Converter
+// {
+
+// }
