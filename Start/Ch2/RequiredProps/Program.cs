d8dc456cba7ba08273f3e199755a71612d78cb2c
ttran375 <137940945+ttran375@users.noreply.github.com>
2023-12-26 18:31:01 +0000

    Required properties

--- Start/Ch2/RequiredProps/Program.cs
+++ Start/Ch2/RequiredProps/Program.cs
@@ -2,12 +2,14 @@
 // The "required" modifier
 
 // Construct some objects using Object Initializer syntax
-Employee emp1 = new Employee() {FirstName="John", LastName="Doe", ID=1, Department="Sales"};
-Employee emp2 = new Employee() {FirstName="Jane", LastName="Deaux", ID=2, Department="R&D"};
-Employee emp3 = new Employee() {FirstName="Jim", LastName="Dough", ID=3, Department="Marketing"};
+// Employee emp1 = new Employee() { FirstName = "John", Department = "Sales" };
+Employee emp1 = new Employee() { FirstName = "John", LastName = "Doe", ID = 1, Department = "Sales" };
+// Employee emp1 = new Employee() { LastName = "Doe", ID = 1, Department = "Sales" };
+Employee emp2 = new Employee() { FirstName = "Jane", LastName = "Deaux", ID = 2, Department = "R&D" };
+Employee emp3 = new Employee() { FirstName = "Jim", LastName = "Dough", ID = 3, Department = "Marketing" };
 
 // TODO: Construct an object using the constructor
-
+Employee emp4 = new Employee("Joe", "Doh", 4, "Marketing");
 
 Console.WriteLine(emp1);
 Console.WriteLine(emp2);
