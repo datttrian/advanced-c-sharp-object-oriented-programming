d5d9c821b64445bd420e0d95e2714c102238172b
ttran375 <137940945+ttran375@users.noreply.github.com>
2023-12-26 19:27:10 +0000

    Abstract methods and properties

--- Start/Ch4/AbstractPropsFuncs/Program.cs
+++ Start/Ch4/AbstractPropsFuncs/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Example file for Advanced C#: Object Oriented Programming by Joe Marini
 
-Car c = new Car() {Make = "Ford", Model = "Escort"};
-Motorcycle m = new Motorcycle() {Make = "Triumph", Model = "Thunderbird"};
+Car c = new Car() { Make = "Ford", Model = "Escort" };
+Motorcycle m = new Motorcycle() { Make = "Triumph", Model = "Thunderbird" };
 
 // Exercise the methods and properties
 Console.WriteLine(c);
--- Start/Ch4/AbstractPropsFuncs/Vehicles.cs
+++ Start/Ch4/AbstractPropsFuncs/Vehicles.cs
@@ -3,27 +3,33 @@
 // Declare an abstract base class to prevent direct instantiation
 public abstract class Vehicle
 {
-    public Vehicle() {
+    public Vehicle()
+    {
     }
 
-    public required string Model {
+    public required string Model
+    {
         get;
         init;
     }
 
-    public required string Make {
+    public required string Make
+    {
         get;
         init;
     }
 
     // By declaring a member to be abstract, we can require subclasses to implement it
-    public virtual void SoundHorn() {
-        Console.WriteLine("Add horn sound here");
-    }
+    // public abstract void SoundHorn() {
+    //     Console.WriteLine("Add horn sound here");
+    // }
+
+    public abstract void SoundHorn();
 
     public virtual int WheelCount { get; }
 
-    public override string ToString() {
+    public override string ToString()
+    {
         return $"{GetType()}: {Make} {Model}, Wheels: {WheelCount}";
     }
 }
@@ -31,10 +37,22 @@ public abstract class Vehicle
 // Declare sublasses that inherit from the abstract class
 public class Car : Vehicle
 {
-    public Car() {}
+    public Car() { }
+
+    public override int WheelCount => 4;
+    public override void SoundHorn()
+    {
+        Console.WriteLine("Beep Beep");
+    }
 }
 
 public class Motorcycle : Vehicle
 {
-    public Motorcycle() {}
+    public Motorcycle() { }
+
+    public override int WheelCount => 2;
+    public override void SoundHorn()
+    {
+        Console.WriteLine("Honk Honk");
+    }
 }
