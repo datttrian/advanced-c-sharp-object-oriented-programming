3811d80c050462cee2885f22e3d4fe558dd49d13
ttran375 <137940945+ttran375@users.noreply.github.com>
2023-12-26 18:34:32 +0000

    Solution: Build an employee class

--- Start/Ch2/Challenge/Program.cs
+++ Start/Ch2/Challenge/Program.cs
@@ -2,8 +2,8 @@
 // Challenge: create a class hierarchy to represent employees
 
 // Test code for the programming challenge
-var Emp1 = new HourlyEmployee() {FullName="John Doe", Department="Sales", ID=1, PayRate=35.0m};
-var Emp2 = new SalariedEmployee() {FullName="Jane Deaux", Department="Marketing", ID=2, Salary=75000.0m};
+var Emp1 = new HourlyEmployee() { FullName = "John Doe", Department = "Sales", ID = 1, PayRate = 35.0m };
+var Emp2 = new SalariedEmployee() { FullName = "Jane Deaux", Department = "Marketing", ID = 2, Salary = 75000.0m };
 
 Console.WriteLine($"{Emp1} -- {Emp1.PayRate:C2}");
 Emp1.Department = "Legal";
